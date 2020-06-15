ee2b0267b39fb55f7fe09baa26022c547e854066
Mamadou <bam76531@gmail.com>
2020-06-15 19:23:51 +0200

    premier modification

--- registration/registration/settings.py
+++ registration/registration/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'registration.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'registration.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'djangodata',
+        'NAME': 'djangodatabase',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
@@ -123,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
