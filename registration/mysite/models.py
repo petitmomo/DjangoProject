5ee4a813de4ab977ae77fe98fbd55b9615ddb56c
Mamadou <bam76531@gmail.com>
2020-06-15 18:59:39 +0200

    premier modification

--- registration/mysite/models.py
+++ registration/mysite/models.py
@@ -1,16 +1,27 @@
 from django.db import models
-# Create your models here.
+from django.contrib.auth.models import User
 
-class Member(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    number = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
 
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
+class Cour(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.TextField()
+    image = models.FileField(blank=True)
+    content = models.TextField()
+    updated_on = models.DateTimeField()
+    created_on = models.DateTimeField()
+    status = models.IntegerField(choices=STATUS, default=0)
+
+   
+    def __str__(self):
+        return self.title
+
+class CourImage(models.Model):
+    cour = models.ForeignKey(Cour, default=None, on_delete=models.CASCADE)
+    image = models.FileField(upload_to='images/')
 
     def __str__(self):
-        return self.first_name
-
-    class Meta:
-        db_table = 'web_member'
+        return self.cour.title
