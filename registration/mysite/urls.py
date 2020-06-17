8efcf5a76638d12f52a8c60c0536a129effdce91
Mamadou <bam76531@gmail.com>
2020-06-17 16:02:12 +0200

    deuxieme commit

--- registration/mysite/urls.py
+++ registration/mysite/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import CourList, CourDetail
+from .views import CourList, CourDetail, CourUpdate, CourDelete
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -10,7 +10,12 @@ urlpatterns = [
     #path('home', views.home, name='home'),
     path('index', views.index, name='index'),
     path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
     path('password_reset', views.password_reset, name='password_reset'),
     path('home', CourList.as_view(), name='cour-index'),
     path('cour-detail/<int:pk>/', CourDetail.as_view(), name='cour-detail'),
+    path('cour-update/<int:pk>/', CourUpdate.as_view(), name='cour-update'),
+    path('cour-delete/<int:pk>/', CourDelete.as_view(), name='cour-delete'),
+
 ]
+
